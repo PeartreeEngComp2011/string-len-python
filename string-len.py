a16ab5305ad6a816da7f46fac34620f7c0290cde
Daniel Gusmao <danielgp20@hotmail.com>
2022-04-08 10:12:02 -0300

    string-len python

---
+++ string-len.py
@@ -0,0 +1,22 @@
+nome = "Daniel"
+sobrenome = "Gusmao"
+
+concatenar = (nome+" "+sobrenome)
+
+tamanho = len(concatenar)
+
+print(tamanho)
+
+#Imprime as primeiras letras
+
+print(nome[0])
+print(nome[1])
+print(nome[2])
+
+#Imprime uma sequencia de caracteres
+
+print(concatenar[0:7])
+
+#Imprime do zero até o final
+
+print(concatenar[0:])
